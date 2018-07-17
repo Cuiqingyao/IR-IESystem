5d4934e51d301f28c1d4bb1d41b26199f4e8d0dd
Cuiqingyao <cuiqingyaowhzz@163.com>
2018-07-17 09:14:49 +0800

    add bootstrap
    添加bootstrap相关文件

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from infoR import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index ),
+    path('search/', views.search),
+    path('detail/(?p<job_id>/d+)/', views.detail),
 ]
+
